0b77536e63fbcac5be0b51942b119e45d31e26a8
Juan Pablo <juan.ospina.vanegas@correounivalle.edu.co>
2025-04-10 19:42:38 -0500

    feat: update file processor UI and logic for palindrome and combinations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import multer from 'multer';
-import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { isPalindrome } from './logic/palindrome.js';
@@ -8,33 +7,33 @@ import { getPermutations } from './logic/combinations.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const rootDir = path.join(__dirname, '..');
 
 const app = express();
-const upload = multer({ dest: path.join(rootDir, 'uploads/') });
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 app.use(express.static(path.join(rootDir, 'public')));
 
 app.post('/upload', upload.single('archivo'), (req, res) => {
   try {
     const problema = req.body.problema;
-    const filePath = req.file.path;
-    const content = fs.readFileSync(filePath, 'utf-8');
+
+    const content = req.file.buffer.toString('utf-8');
     let resultado;
 
     if (problema === '1') {
-      console.log('Contenido del archivo:', content); // Debugging
+      console.log('IsPalindrome: Contenido del archivo:', content);
       resultado = isPalindrome(content);
     } else if (problema === '2') {
-      console.log('Contenido del archivo:', content); // Debugging
+      console.log('Fiesta: Contenido del archivo:', content);
       resultado = getPermutations(content);
     } else {
       return res.status(400).json({ error: 'Problema no válido' });
     }
 
-    fs.unlinkSync(filePath); // Limpieza del archivo temporal
-    res.json({ resultado: 'esta por venir' });
+    res.json({ resultado });
   } catch (err) {
     res.status(500).json({ error: 'Error procesando el archivo: ' + err.message });
   }
