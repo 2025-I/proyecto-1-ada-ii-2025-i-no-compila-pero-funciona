7a7251a6f01446b80f34e084bd561f1318482121
Juan Pablo <juan.ospina.vanegas@correounivalle.edu.co>
2025-04-12 17:29:43 -0500

    Add ESLint configuration and linting script to package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
 
 export default defineConfig([
+  js.configs.recommended,
   {
     files: ['**/*.js'],
     ignores: ['node_modules/**'],
@@ -12,35 +15,35 @@ export default defineConfig([
         ...globals.es2021,
       },
       ecmaVersion: 2022,
-      sourceType: 'module', // Para soporte de ES Modules
+      sourceType: 'module',
     },
     plugins: {
-      js,
+      import: eslintPluginImport,
+      prettier,
     },
-    extends: ['eslint:recommended', 'plugin:js/recommended', 'prettier'],
     rules: {
-      // Reglas base de ESLint
-      'no-console': 'warn',
-      'no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-      ],
-      'no-underscore-dangle': 'off',
-
-      // Reglas para Node.js/ES Modules
+      // Import plugin rules
       'import/extensions': ['error', 'always', { ignorePackages: true }],
+
+      // Base ESLint rules
+      'no-console': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-underscore-dangle': 'off',
       'node/no-unsupported-features/es-syntax': 'off',
 
-      // Reglas de estilo (complementan a Prettier)
+      // Style rules
       semi: ['error', 'always'],
       quotes: ['error', 'single', { avoidEscape: true }],
       indent: ['error', 2],
       'comma-dangle': ['error', 'only-multiline'],
 
-      // Mejores prácticas
+      // Best practices
       'prefer-const': 'error',
       'arrow-body-style': ['error', 'as-needed'],
       'no-var': 'error',
+
+      // Prettier
+      'prettier/prettier': 'error',
     },
   },
 ]);
